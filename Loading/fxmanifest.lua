289e5587dbb5daa7bebc24ebab7e217ec9f5a4a7
Adico <69988400+Adiicoo@users.noreply.github.com>
2023-01-05 20:57:50 +0100

    Add files via upload

---
+++ Loading/fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author 'AZ3T3KFR GAME (Multi développement)'
+description 'Discord : https://discord.gg/K3BRxapgWn'
+version '1.0.0'
+
+loadscreen 'index.html'
+
+files {
+    'index.html',
+    'css/style.css',
+    'js/script.js',
+    'img/logo.gif',
+    'img/staff1.png',
+    'img/staff2.png',
+    'img/staff3.png',
+    'img/staff4.png',
+    'img/staff5.png',
+    'img/staff6.png',
+    'img/staff7.png',
+    'img/metier/ambulancier.png',
+    'img/metier/brinks.png',
+    'img/metier/conces.png',
+    'img/metier/fib.png',
+    'img/metier/lspd.png',
+    'img/metier/taxi.png',
+    'img/metier/vignerons.png',
+    'music/music.mp3',
+    'video.js'
+}
